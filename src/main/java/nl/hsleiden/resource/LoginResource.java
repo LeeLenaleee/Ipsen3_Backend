80587a6c6c9588e36bf500e443e7201e4afc74e4
Jacco van den Berg <jaccoberg2281@gmail.com>
2018-12-18 10:41:29 +0100

    backend om user te laten inloggen af

--- src/main/java/nl/hsleiden/resource/LoginResource.java
+++ src/main/java/nl/hsleiden/resource/LoginResource.java
@@ -1,13 +1,16 @@
 package nl.hsleiden.resource;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import nl.hsleiden.View;
+import nl.hsleiden.model.LoginUser;
+import nl.hsleiden.model.User;
+import nl.hsleiden.persistence.UserDAO;
 import nl.hsleiden.service.LoginService;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.validation.Valid;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
 import java.util.Optional;
@@ -20,20 +23,19 @@ import java.util.Optional;
 @Path("/login")
 public class LoginResource {
     private final LoginService service;
+    private final UserDAO userDAO;
 
     @Inject
-    public LoginResource(LoginService service) {
+    public LoginResource(LoginService service, UserDAO userDAO) {
         this.service = service;
+        this.userDAO = userDAO;
     }
 
-    @GET
+    /*@GET
     @Produces(MediaType.TEXT_PLAIN)
     public String get(@QueryParam("email") Optional<String> email, @QueryParam("password") Optional<String> password) {
         String displayName;
         String testMail = "jaccoberg2281@gmail.com";
-        System.out.println("hi"+ email.get().equals(testMail));
-        String x = email.toString();
-        System.out.println(testMail.equals(x));
         if (email.get().equals(testMail) && password.get().equals("lol")) {
             displayName = Boolean.toString(true);
         } else {
@@ -42,5 +44,36 @@ public class LoginResource {
 
         //return "Hello " + displayName;
         return "[{\"credentials\": \"" + displayName + "\"}]";
+    }*/
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JsonView(View.Protected.class)
+    public User create( LoginUser loginUser)
+    {
+        System.out.println(loginUser.getEmail());
+        if(loginUser.getPassword().equals(this.userDAO.getByEmailAddress(loginUser.getEmail()).getPassword())){
+            return this.userDAO.getByEmailAddress("first@user.com");
+        }
+        else {
+            //TODO hier een 400 bad request returnen
+            return null;
+        }
     }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JsonView(View.Protected.class)
+    public User get(@QueryParam("email") String email, @QueryParam("password") String password)
+    {
+        if(email.equals(this.userDAO.getByEmailAddress("first@user.com").getEmailAddress())){
+            System.out.println("idk what");
+            return this.userDAO.getByEmailAddress(email);
+        }
+        else {
+            //TODO hier een 400 bad request returnen
+            return null;
+        }
+    }
+
 }
--- src/main/java/nl/hsleiden/service/LoginService.java
+++ src/main/java/nl/hsleiden/service/LoginService.java
@@ -1,57 +1,37 @@
 package nl.hsleiden.service;
 
+import nl.hsleiden.model.LoginUser;
 import nl.hsleiden.model.User;
 import nl.hsleiden.persistence.LoginDao;
+import nl.hsleiden.persistence.UserDAO;
 
 import javax.inject.Inject;
 import java.util.Collection;
 
 public class LoginService extends BaseService<User>{
     private final LoginDao dao;
+    private final UserDAO userDao;
+    User user;
 
     @Inject
-    public LoginService(LoginDao dao)
+    public LoginService(LoginDao dao, UserDAO userDAO)
     {
         this.dao = dao;
+        this.userDao = userDAO;
     }
 
-    /*public Collection<User> getAll()
-    {
-        return dao.getAll();
-    }
-
-    public User get(int id)
-    {
-        return requireResult(dao.get(id));
-    }
-
-    public void add(User user)
-    {
-        user.setRoles(new String[] { "GUEST" });
-
-        dao.add(user);
-    }
-
-    public void update(User authenticator, int id, User user)
-    {
-        // Controleren of deze gebruiker wel bestaat
-        User oldUser = get(id);
-
-        if (!authenticator.hasRole("ADMIN"))
-        {
-            // Vaststellen dat de geauthenticeerde gebruiker
-            // zichzelf aan het aanpassen is
-            assertSelf(authenticator, oldUser);
+    public boolean checkLogin(LoginUser loginUser){
+        LoginUser check = dao.getUser(loginUser.getEmail());
+        if (loginUser.getPassword().equals(check.getPassword())){
+            this.user = userDao.getByEmailAddress(loginUser.getEmail());
+            return true;
+        }else {
+            return false;
         }
-
-        dao.update(id, user);
     }
 
-    public void delete(int id)
-    {
-        // Controleren of deze gebruiker wel bestaat
-        User user = get(id);
+    public User getUser(){
+        return this.user;
 
-        dao.delete(id);
-    }*/
+    }
 }
