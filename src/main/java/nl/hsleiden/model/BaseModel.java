10d7bd4384e491675ce8321c3f54231106680a72
Simon <nomisvank@hotmail.com>
2019-01-07 12:04:27 +0100

    onkosten

--- src/main/java/nl/hsleiden/model/BaseModel.java
+++ src/main/java/nl/hsleiden/model/BaseModel.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @MappedSuperclass
 public class BaseModel {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
