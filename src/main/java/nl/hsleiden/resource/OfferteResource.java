0265a28344ce8339dafa7d6497b8597f3f3e458a
Jacco van den Berg <jaccoberg2281@gmail.com>
2019-01-09 15:15:39 +0100

    offerte kan gedownload worden

--- src/main/java/nl/hsleiden/resource/OfferteResource.java
+++ src/main/java/nl/hsleiden/resource/OfferteResource.java
@@ -11,57 +11,23 @@ import javax.inject.Singleton;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
 import java.util.List;
 
 @Singleton
 @Path("/offerte")
 @Produces(MediaType.APPLICATION_JSON)
 public class OfferteResource {
-    private final OfferteService service;
+    private final OfferteService offerteService;
 
     @Inject
-    public OfferteResource(OfferteService service) { this.service = service; }
+    public OfferteResource(OfferteService offerteService){this.offerteService = offerteService;}
 
-    @GET
-    @Path("/{id}")
-    @UnitOfWork
-    @JsonView(View.Protected.class)
-    public OfferteModel findById(@PathParam("id") int id) {
-        return service.findById(id);
-    }
-
+    @Path("/pdf")
     @GET
     @UnitOfWork
-    @JsonView(View.Protected.class)
-    public List<OfferteModel> findAll() {
-        return service.findAll();
-    }
-
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @UnitOfWork
-    @JsonView(View.Public.class)
-    public void create(@Valid OfferteModel OfferteModel)
-    {
-        service.create(OfferteModel);
-    }
-
-    @DELETE
-    @Consumes(MediaType.APPLICATION_JSON)
-    @UnitOfWork
-    @JsonView(View.Public.class)
-    public void delete(@Valid OfferteModel OfferteModel)
-    {
-        service.delete(OfferteModel);
-    }
-
-    @PUT
-    @Path("/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @UnitOfWork
-    @JsonView(View.Public.class)
-    public void update(@PathParam("id") int id, @Valid OfferteModel user)
-    {
-        service.update(user, id);
+    @Produces({"application/pdf"})
+    public File getFile(@QueryParam("id") int id) {
+        return this.offerteService.getById(id);
     }
 }
--- src/main/java/nl/hsleiden/service/OfferteService.java
+++ src/main/java/nl/hsleiden/service/OfferteService.java
@@ -2,21 +2,25 @@ package nl.hsleiden.service;
 
 import nl.hsleiden.model.OfferteModel;
 import nl.hsleiden.persistence.OfferteDAO;
+import nl.hsleiden.utility.PDFWriter;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.ws.rs.NotFoundException;
+import java.io.File;
+import java.util.Optional;
 
 @Singleton
-public class OfferteService extends BaseService<OfferteModel, OfferteDAO> {
+public class OfferteService {
+    OfferteDAO offerteDAO;
 
     @Inject
-    public OfferteService(OfferteDAO dao)
-    {
-        super(dao);
-    }
+    public OfferteService(OfferteDAO dao){this.offerteDAO = dao;}
 
-    public void update(OfferteModel user, int userId) {
-        user.setId(userId);
-        super.update(user);
+    public File getById(int offerteId) {
+        Optional<OfferteModel> offerte = offerteDAO.getByOfferteId(offerteId);
+        if(offerte.isPresent()){
+            return PDFWriter.maakOfferte(offerte.get());
+        }else throw new NotFoundException("Not found");
     }
 }
