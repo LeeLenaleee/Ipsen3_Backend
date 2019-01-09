3e606596d86726498a4449fcefebda2e1c2124ba
Jacco van den Berg <jaccoberg2281@gmail.com>
2019-01-09 14:58:02 +0100

    code nog ff wat netter gemaakt

--- src/main/java/nl/hsleiden/service/FactuurService.java
+++ src/main/java/nl/hsleiden/service/FactuurService.java
@@ -5,19 +5,21 @@ import nl.hsleiden.model.FactuurModel;
 import nl.hsleiden.persistence.FactuurDAO;
 
 import javax.inject.Inject;
+import javax.ws.rs.NotFoundException;
 import java.io.File;
 import java.util.Optional;
 
 
 public class FactuurService {
     FactuurDAO factuurDAO;
-    FactuurModel factuur;
 
     @Inject
     public FactuurService(FactuurDAO dao){this.factuurDAO = dao;}
 
     public File getById(int factuurId) {
-        FactuurModel factuurModel = factuurDAO.getByFactuurId(factuurId).get();
-        return PDFWriter.maakFactuur(factuurModel);
+        Optional<FactuurModel> factuur = factuurDAO.getByFactuurId(factuurId);
+        if(factuur.isPresent()){
+            return PDFWriter.maakFactuur(factuur.get());
+        }else throw new NotFoundException("Not found");
     }
 }
