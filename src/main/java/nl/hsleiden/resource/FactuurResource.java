4562bc544b9a8be48a8629778267ba30522f7270
Jacco van den Berg <jaccoberg2281@gmail.com>
2019-01-17 17:02:14 +0100

    bezig met factuur downloaden

--- src/main/java/nl/hsleiden/resource/FactuurResource.java
+++ src/main/java/nl/hsleiden/resource/FactuurResource.java
@@ -13,11 +13,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 import java.io.File;
 
 @Singleton
 @Path("/factuur")
-@RolesAllowed("USER")
+//@RolesAllowed("USER")
 public class FactuurResource extends BaseResource<FactuurModel, FactuurDAO, FactuurService> {
 
     @Inject
@@ -25,12 +26,24 @@ public class FactuurResource extends BaseResource<FactuurModel, FactuurDAO, Fact
         super(service);
     }
 
-    @Path("/download")
+    /*@Path("/download")
     @GET
     @UnitOfWork
     @Produces({"application/pdf"})
     public File getFile(@QueryParam("id") int id) {
         return PDFWriter.maakFactuur(super.findById(id));
+    }*/
+
+    @Path("/download")
+    @GET
+    @UnitOfWork
+    @Produces({"application/pdf"})
+    public Response getFile(@QueryParam("id") int id) {
+        File file = PDFWriter.maakFactuur(super.findById(id));
+        Response.ResponseBuilder response = Response.ok(file);
+        response.header("Content-Disposition","attachment; filename=test.pdf");
+        System.err.println("kap");
+        return response.build();
     }
 
 }
