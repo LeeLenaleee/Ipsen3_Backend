fdbf5105925c0b951c54b6ad5226b427ebb2578b
Jacco van den Berg <jaccoberg2281@gmail.com>
2019-01-22 17:56:03 +0100

    offerte download

--- src/main/java/nl/hsleiden/resource/OfferteResource.java
+++ src/main/java/nl/hsleiden/resource/OfferteResource.java
@@ -1,6 +1,8 @@
 package nl.hsleiden.resource;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import io.dropwizard.hibernate.UnitOfWork;
+import nl.hsleiden.View;
 import nl.hsleiden.model.OfferteModel;
 import nl.hsleiden.persistence.OfferteDAO;
 import nl.hsleiden.service.OfferteService;
@@ -13,7 +15,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
 import java.io.File;
+import java.util.List;
 
 @Singleton
 @Path("/offerte")
@@ -31,5 +36,14 @@ public class OfferteResource extends BaseResource<OfferteModel, OfferteDAO, Offe
     @Produces({"application/pdf"})
     public File getFile(@QueryParam("id") int id) {
         return PDFWriter.maakOfferte(super.findById(id));
+    }
+
+    @GET
+    @Path("/zoek/")
+    @UnitOfWork
+    @JsonView(View.Protected.class)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<OfferteModel> findByOmschrijving(@QueryParam("correspondentie") int correspondentie) {
+        return service.findByCorrespondentieNummer(correspondentie);
     }
 }
--- src/main/java/nl/hsleiden/service/OfferteService.java
+++ src/main/java/nl/hsleiden/service/OfferteService.java
@@ -5,11 +5,16 @@ import nl.hsleiden.persistence.OfferteDAO;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
 public class OfferteService extends BaseService<OfferteModel, OfferteDAO> {
     @Inject
     public OfferteService(OfferteDAO dao) {
         super(dao);
+    }
+
+    public List<OfferteModel> findByCorrespondentieNummer(int correspondentie) {
+        return dao.findByCorrespondentieNummer(correspondentie);
     }
 }
